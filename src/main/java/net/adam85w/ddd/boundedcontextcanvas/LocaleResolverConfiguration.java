a5591c74968f942bf80712f8efdff5f30627e578
Adam Woźniak <adam85.w@gmail.com>
2024-04-28 20:11:31 +0200

    Added the english locale resolver as a static resolver and fixed maven resources mapping

---
+++ src/main/java/net/adam85w/ddd/boundedcontextcanvas/LocaleResolverConfiguration.java
@@ -0,0 +1,17 @@
+package net.adam85w.ddd.boundedcontextcanvas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+class LocaleResolverConfiguration {
+
+    @Bean
+    public LocaleResolver localResolver() {
+        return new FixedLocaleResolver(Locale.ENGLISH);
+    }
+}
