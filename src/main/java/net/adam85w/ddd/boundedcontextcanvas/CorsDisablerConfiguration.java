b738523fcb2da0db00e2f5b85ec65c4c55c24ccc
Adam Woźniak <adam85.w@gmail.com>
2024-04-28 20:12:57 +0200

    Added the CORS disabler to enable more opportunities for integration with external tools

---
+++ src/main/java/net/adam85w/ddd/boundedcontextcanvas/CorsDisablerConfiguration.java
@@ -0,0 +1,16 @@
+package net.adam85w.ddd.boundedcontextcanvas;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnProperty(value = "security.cors.disabled", havingValue = "true")
+class CorsDisablerConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
